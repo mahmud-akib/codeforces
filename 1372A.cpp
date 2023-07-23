8a2a07acf41276204ed4faaf70e524947a45eb26
Akib Mahmud <101253688+mahmud-akib@users.noreply.github.com>
2023-07-23 22:10:56 +0600

    Create 1372A.cpp

---
+++ 1372A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int t,n;
+    cin>>t;
+    while(t--){
+        int ans = 0;
+        cin>>n;
+        ans = n/2;
+        if(n % 2 == 1)ans++;
+
+        cout<<ans<<endl;
+
+    }
+
+    return 0;
+}
